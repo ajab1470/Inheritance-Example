eb321a2dbaf07d98826fc2749a1ef58173737854
Ashley <ajab1470@gmail.com>
2020-02-07 14:51:25 -0500

    Initial commit - from previous project. Nothing useful in this version

---
+++ src/Animal.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+public abstract class Animal {
+    //the name of this animal
+    protected String name;
+    //the length in inches of this animal
+    protected int length;
+    //
+    protected String color;
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat0 = new Cat("Fluffy", 12, 35.7);
+        Cat cat1 = new Cat("Kyra", 16, 19.2);
+        Cat cat2 = new Cat("Pebbles", 19, 34);
+
+        cat0.meow();
+        cat1.meow();
+    }
+}
